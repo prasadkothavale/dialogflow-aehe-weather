1546da42df9fbb2a0256e2cf79bc866433dbf9c2
Prasad Kothavale <prasadkothavale@yahoo.co.in>
2018-01-14 21:54:36 +0530

    Convert to express js project

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/helloHttp', function (req, res, next) {
+  let response = 'This is sample GET response from webhook';
+
+  res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
+  res.send(JSON.stringify({
+    "speech": response, "displayText": response
+    //"speech" is the spoken version of the response, "displayText" is the visual version
+  }));
+})
+
+router.post('/helloHttp', function (req, res, next) {
+  let response = 'This is sample POST response from webhook';
+
+  res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
+  res.send(JSON.stringify({
+    "speech": response, "displayText": response
+    //"speech" is the spoken version of the response, "displayText" is the visual version
+  }));
+})
+
+module.exports = router;
